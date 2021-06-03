7ce6e98ec7634f003e6ea53bf4a6120c7e5fabf7
Tim Perry <pimterry@gmail.com>
2021-06-03 17:28:00 +0200

    Add a non-Mac integration test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,9 @@ import { expect } from 'chai';
 import { getMacSystemProxy } from '../src/index';
 
 describe("mac-system-proxy", () => {
-    it("can get the Mac system proxy", async () => {
+    it("can get the Mac system proxy", async function () {
+        if (process.platform !== 'darwin') return this.skip();
+
         expect(
             await getMacSystemProxy()
         ).to.deep.equal({
@@ -12,5 +14,14 @@ describe("mac-system-proxy", () => {
             ],
             "FTPPassive": "1"
         }); // Seems to be the default config, matches GitHub Actions runner at least
+    });
+
+    it("rejects the promise on non-Mac platforms", async function () {
+        if (process.platform === 'darwin') return this.skip();
+
+        const result = await getMacSystemProxy().catch(e => e);
+
+        expect(result).to.be.instanceOf(Error);
+        expect(result.message).to.include("spawn scutil");
     });
 });
