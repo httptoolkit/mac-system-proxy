b7996fef9f465f1613ec4c7bcff00b5b0f7c8b96
Tim Perry <pimterry@gmail.com>
2021-06-03 16:44:07 +0200

    Add a very rough basic scutil output parser

--- src/parse-scutil.ts
+++ src/parse-scutil.ts
@@ -1,2 +1,35 @@
+const TYPE_KEY = "__scutil__type__";
+
+// Quick hacky parser, which translates output into valid JSON:
 export function parseScutilOutput(output: string) {
+    try {
+        const jsonFormattedString = output
+            // Reduce type markers to just an inline __scutil__type__ marker on array objects:
+            .replace(/<dictionary>/g, '')
+            .replace(/<array> {/g, `{ ${TYPE_KEY} : array`)
+            .split(/\s+/)
+            .map((token) => {
+                if (!token || token === ':' || token === '{' || token === '}') return token;
+                else return '"' + token + '"';
+            })
+            .join(' ')
+            .replace(/" "/g, '", "')
+            .replace(/} "/g, '}, "');
+
+        const data = JSON.parse(jsonFormattedString, (key, value) => {
+            if (value[TYPE_KEY] === 'array') {
+                delete value[TYPE_KEY];
+                return Object.values(value);
+            } else {
+                return value;
+            }
+        });
+
+        return data;
+    } catch (e) {
+        throw Object.assign(
+            new Error("Unexpected scutil proxy output format"),
+            { scutilOutput: output } // Attach output for debugging elsewhere
+        );
+    }
 }
--- test/parse-scutil.spec.ts
+++ test/parse-scutil.spec.ts
@@ -1,17 +1,158 @@
+import { expect } from 'chai';
 import { parseScutilOutput } from '../src/parse-scutil';
 
 describe("Scutil Parsing", () => {
+
     it("should parse an empty proxy configuration", () => {
         const parsed = parseScutilOutput(
-`
-`
-        );
+`<dictionary> {
+}`);
+
+        expect(parsed).to.deep.equal({});
     });
 
-    it("should parse a complete proxy configuration", () => {
+    it("should parse a disabled proxy configuration", () => {
         const parsed = parseScutilOutput(
-`
-`
-        );
+`<dictionary> {
+    HTTPEnable : 0
+    HTTPSEnable : 0
+}`);
+
+        expect(parsed).to.deep.equal({
+            HTTPEnable: "0",
+            HTTPSEnable: "0"
+        });
+    });
+
+    it("should parse an empty proxy configuration with an exceptions array", () => {
+        const parsed = parseScutilOutput(
+`<dictionary> {
+    ExceptionsList : <array> {
+        0 : localhost
+        1 : 127.0.0.1
+    }
+    ExcludeSimpleHostnames : 1
+    HTTPEnable : 0
+    HTTPSEnable : 0
+}`);
+
+        expect(parsed).to.deep.equal({
+            ExceptionsList: ['localhost', '127.0.0.1'],
+            ExcludeSimpleHostnames: "1",
+            HTTPEnable: "0",
+            HTTPSEnable: "0"
+        });
+    });
+
+    it("should parse an HTTP and HTTPS proxy configuration", () => {
+        const parsed = parseScutilOutput(
+`<dictionary> {
+    ExcludeSimpleHostnames : 1
+    HTTPEnable : 1
+    HTTPPort : 8000
+    HTTPProxy : 127.0.0.1
+    HTTPSEnable : 1
+    HTTPSPort : 8443
+    HTTPSProxy : 127.0.0.1
+    HTTPSUser : user
+    SOCKSEnable : 0
+    SOCKSUser : user
+}`);
+
+        expect(parsed).to.deep.equal({
+            "ExcludeSimpleHostnames": "1",
+            "HTTPEnable": "1",
+            "HTTPPort": "8000",
+            "HTTPProxy": "127.0.0.1",
+            "HTTPSEnable": "1",
+            "HTTPSPort": "8443",
+            "HTTPSProxy": "127.0.0.1",
+            "HTTPSUser": "user",
+            "SOCKSEnable": "0",
+            "SOCKSUser": "user"
+        });
+    });
+
+    it("should parse an WPAD PAC proxy configuration", () => {
+        const parsed = parseScutilOutput(
+`<dictionary> {
+    ExceptionsList : <array> {
+        0 : localhost
+        1 : 127.0.0.1
+    }
+    ExcludeSimpleHostnames : 1
+    HTTPEnable : 0
+    HTTPSEnable : 0
+    ProxyAutoConfigEnable : 1
+    ProxyAutoConfigURLString : http://wpad/wpad.dat
+    ProxyAutoDiscoveryEnable : 1
+}`);
+
+        expect(parsed).to.deep.equal({
+            ExceptionsList: ['localhost', '127.0.0.1'],
+            ExcludeSimpleHostnames: "1",
+            HTTPEnable: "0",
+            HTTPSEnable: "0",
+            ProxyAutoConfigEnable: "1",
+            ProxyAutoConfigURLString: "http://wpad/wpad.dat",
+            ProxyAutoDiscoveryEnable: "1"
+        });
+    });
+
+    it("should parse a SOCKS proxy configuration", () => {
+        const parsed = parseScutilOutput(
+`<dictionary> {
+    ExcludeSimpleHostnames : 1
+    HTTPEnable : 0
+    HTTPSEnable : 0
+    SOCKSEnable : 1
+    SOCKSPort : 2020
+    SOCKSProxy : 127.0.0.1
+    SOCKSUser : user
+}`);
+
+        expect(parsed).to.deep.equal({
+            "ExcludeSimpleHostnames": "1",
+            "HTTPEnable": "0",
+            "HTTPSEnable": "0",
+            "SOCKSEnable": "1",
+            "SOCKSPort": "2020",
+            "SOCKSProxy": "127.0.0.1",
+            "SOCKSUser": "user",
+        });
+    });
+
+    it("should parse an explicit PAC configuration", () => {
+        const parsed = parseScutilOutput(
+`<dictionary> {
+    ExcludeSimpleHostnames : 1
+    HTTPEnable : 0
+    HTTPSEnable : 0
+    ProxyAutoConfigEnable : 1
+    ProxyAutoConfigURLString : http://example.com/proxy.pac
+}`);
+
+        expect(parsed).to.deep.equal({
+            "ExcludeSimpleHostnames": "1",
+            "HTTPEnable": "0",
+            "HTTPSEnable": "0",
+            "ProxyAutoConfigEnable": "1",
+            "ProxyAutoConfigURLString": "http://example.com/proxy.pac"
+        });
+    });
+
+    it("should clearly error given invalid input", () => {
+        expect(() =>
+            parseScutilOutput(`{`)
+        ).to.throw('Unexpected scutil proxy output format');
+    });
+
+    it("should clearly error given invalid input", () => {
+        expect(() =>
+            parseScutilOutput(
+`<dictionary> {
+    IncompleteValue :
+}`)
+        ).to.throw('Unexpected scutil proxy output format');
     });
 });
