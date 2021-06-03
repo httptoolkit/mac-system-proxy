0c97520e09d716f0974996ee8034a5dec0f39243
Tim Perry <pimterry@gmail.com>
2021-06-03 17:02:50 +0200

    Add a full test case, configured to match proxy settings in GHA

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,16 @@
+import { expect } from 'chai';
 import { getMacSystemProxy } from '../src/index';
 
 describe("mac-system-proxy", () => {
-    it("can get the Mac system proxy", () => {
-        getMacSystemProxy();
+    it("can get the Mac system proxy", async () => {
+        expect(
+            await getMacSystemProxy()
+        ).to.deep.equal({
+            "ExceptionsList": [
+              "*.local",
+              "169.254/16"
+            ],
+            "FTPPassive": "1"
+        }); // Seems to be the default config, matches GitHub Actions runner at least
     });
 });
